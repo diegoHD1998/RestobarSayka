e08915793dcb8d9204ff8991a310bbc8f94c1a82
Daniel San Martín <daniel.sanmartin@dbnetcorp.com>
2023-03-01 12:43:47 -0300

    Subir  imagen

--- RestobarSayka/RestobarSayka/Controllers/ProductosController.cs
+++ RestobarSayka/RestobarSayka/Controllers/ProductosController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -137,7 +138,63 @@ namespace RestobarSayka.Controllers
 
             return Ok(id);
         }
+        [HttpPost("CargarImagen")]
+        public Task<bool> UploadFile()
+        {
+            //Variable que retorna el valor del resultado del metodo
+            //El valor predeterminado es Falso (false)
+            bool resultado = false;
 
+            //La variable "file" recibe el archivo en el objeto Request.Form
+            //Del POST que realiza la aplicacion a este servicio.
+            //Se envia un formulario completo donde uno de los valores es el archivo
+            var file = Request.Form.Files[0];
+
+            //Variable donde se coloca la ruta relativa de la carpeta de destino
+            //del archivo cargado
+            string RutaCompleta = "C:\\Users\\diego\\Desktop\\RestobarSayka\\FrontEnd2\\public\\assets\\layout\\images\\";
+            //string RutaCompleta = "C:\\DBNET-GITHUB\\RestobarSayka\\RestobarSayka\\RestobarSayka\\imagen\\";
+            //Variable donde se coloca la ruta raíz de la aplicacion
+            //para esto se emplea la variable "_env" antes de declarada
+            // string RutaRaiz = _env.ContentRootPath;
+
+            //Se concatena las variables "RutaRaiz" y "NombreCarpeta"
+            //en una otra variable "RutaCompleta"
+            //string RutaCompleta = RutaRaiz + NombreCarpeta;
+
+
+            //Se valida con la variable "RutaCompleta" si existe dicha carpeta            
+            if (!Directory.Exists(RutaCompleta))
+            {
+                //En caso de no existir se crea esa carpeta
+                Directory.CreateDirectory(RutaCompleta);
+            }
+
+            //Se valida si la variable "file" tiene algun archivo
+            if (file.Length > 0)
+            {
+                //Se declara en esta variable el nombre del archivo cargado
+                string NombreArchivo = file.FileName;
+
+                //Se declara en esta variable la ruta completa con el nombre del archivo
+                string RutaFullCompleta = Path.Combine(RutaCompleta, NombreArchivo);
+
+                //Se crea una variable FileStream para carlo en la ruta definida
+                using (var stream = new FileStream(RutaFullCompleta, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+
+                    //Como se cargo correctamente el archivo
+                    //la variable "resultado" llena el valor "true"
+                    resultado = true;
+                }
+
+            }
+
+            //Se retorna la variable "resultado" como resultado de una tarea
+            return Task.FromResult(resultado);
+
+        }
         private bool ProductoExists(int id)
         {
             return _context.Productos.Any(e => e.IdProducto == id);
